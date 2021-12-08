3b10b4cdb30f8e9a006c2af0384761d9ff86a3e0
DeniskaButkevich <deniskabutkevich@gmail.com>
2021-12-08 16:59:17 +0300

    trying to create a database

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO persons (id, firstname, lastname, age) VALUES
+  ('2', 'aaa', 'bbb',18),
+  ('3', 'ivan', 'ivanov', 28);
---
+++ target/classes/data.sql
@@ -0,0 +1,13 @@
+INSERT INTO persons (id, firstname, lastname, age) VALUES
+  ('2', 'aaa', 'bbb',18),
+  ('3', 'ivan', 'ivanov', 28);
+
+
+
+--  CREATE TABLE persons (
+--    id INT8 NOT NULL PRIMARY KEY,
+--    id_groups INT8,
+--    firstname VARCHAR(256),
+--    lastname VARCHAR(256),
+--    age SMALLINT
+--  );
