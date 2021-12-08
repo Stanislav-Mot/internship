3c5f61283f6f8f16fd6654a1db63ca74e5543e1d
DeniskaButkevich <deniskabutkevich@gmail.com>
2021-12-08 17:17:37 +0300

    added simple GRUD for ProgressController and PersonController

---
+++ src/main/java/com/internship/internship/controllers/GroupTasksController.java
@@ -0,0 +1,7 @@
+package com.internship.internship.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GroupTasksController {
+}
--- src/main/java/com/internship/internship/models/Person.java
+++ src/main/java/com/internship/internship/models/Person.java
@@ -1,12 +1,56 @@
 package com.internship.internship.models;
 
-import java.util.LinkedList;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String firstname;
     private String lastname;
     private Integer age;
     private GroupTasks groupTasks;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public GroupTasks getGroupTasks() {
+        return groupTasks;
+    }
+
+    public void setGroupTasks(GroupTasks groupTasks) {
+        this.groupTasks = groupTasks;
+    }
 }
